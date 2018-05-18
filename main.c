fc33643ac24c9fd52ae2b0a208a5792c60eb5fee
0452014 <u0452014@nkfust.edu.tw>
2018-05-18 15:15:55 +0800

    commit2

--- main.c
+++ main.c
@@ -4,7 +4,13 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	int i,sum,a;
 	printf("hi\n");
-	printf("i am 0452014");
+	printf("i am 0452014\n");
+	sum=0;//sum的初始值 
+	for(i=0;i<10;i++){
+		sum=sum+i;
+	}
+	printf("sum=%d\n",sum);//印出輸出總和 
 	return 0;
 }
