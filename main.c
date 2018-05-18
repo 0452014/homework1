d89d7eedc68ef36fb2e7bb0f2f5d8f7349b8c870
0452014 <u0452014@nkfust.edu.tw>
2018-05-18 15:10:44 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("hi\n");
+	printf("i am 0452014");
+	return 0;
+}
